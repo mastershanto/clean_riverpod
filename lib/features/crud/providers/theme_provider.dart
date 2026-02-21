0dc15804478faba7b7ba7161e379e7089bba5abe
masterShanto <mastershanto@gmail.com>
2026-02-21 19:59:10 +0600

    feat: Update dependencies and add new packages for improved functionality

--- lib/features/crud/providers/theme_provider.dart
+++ lib/features/crud/providers/theme_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
   return ThemeNotifier();
--- lib/providers/locale_provider.dart
+++ lib/providers/locale_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 class LocaleNotifier extends StateNotifier<Locale> {
   LocaleNotifier() : super(const Locale('en'));
